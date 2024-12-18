d2ad4eb46f2f8423f1910011388e0fd99b935ba1
Max <kavun.onficial@gmail.com>
2024-12-18 17:56:21 +0100

    feat: add CurrencyContentTypeFilter to set content-type and character encoding for currency requests

---
+++ src/main/java/com/maxkavun/filter/CurrencyContentTypeFilter.java
@@ -0,0 +1,29 @@
+package com.maxkavun.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter("/currencies/*")
+public class CurrencyContentTypeFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        if ("POST".equalsIgnoreCase(request.getMethod())) {
+            response.setContentType("application/x-www-form-urlencoded");
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+}

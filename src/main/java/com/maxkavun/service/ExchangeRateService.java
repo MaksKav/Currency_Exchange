4739743559e3b199a36e3d2af353566dd0a81722
Max <kavun.onficial@gmail.com>
2024-12-27 12:24:38 +0100

    feat: implement addNewExchangeRate method to insert exchange rates

--- src/main/java/com/maxkavun/service/ExchangeRateService.java
+++ src/main/java/com/maxkavun/service/ExchangeRateService.java
@@ -3,10 +3,13 @@ package com.maxkavun.service;
 import com.maxkavun.dao.CurrencyDao;
 import com.maxkavun.dao.ExchangeRateDao;
 import com.maxkavun.dto.ExchangeRateDto;
+import com.maxkavun.exception.BusinessException;
+import com.maxkavun.exception.CurrencyNotFoundException;
 import com.maxkavun.mapper.ExchangeRateMapper;
 import com.maxkavun.model.Currency;
 import com.maxkavun.model.ExchangeRate;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -34,6 +37,23 @@ public class ExchangeRateService {
     public Optional<ExchangeRateDto> getExchangeRateByCode(String code) {
         return exchangeRateDao.findByCode(code)
                 .flatMap(this::mapToDtoWithCurrencies);
+    }
+
+    public ExchangeRateDto addNewExchangeRate(String baseCurrencyCode , String targetCurrencyCode , BigDecimal rate) {
+        Currency baseCurrency = currencyDao.findByCode(baseCurrencyCode)
+                .orElseThrow(() -> new CurrencyNotFoundException("Base currency not found in database " + baseCurrencyCode));
+        Currency targetCurrency = currencyDao.findByCode(targetCurrencyCode)
+                .orElseThrow(() -> new CurrencyNotFoundException("Target currency not found in database " + targetCurrencyCode));
+
+        String baseAndTargetCode = baseCurrencyCode + targetCurrencyCode;
+        Optional<ExchangeRate> exchangeRateDto = exchangeRateDao.findByCode(baseAndTargetCode);
+        if (exchangeRateDto.isPresent()){
+            throw new BusinessException("This exchange rate is exist " + exchangeRateDto.get());
+        }
+
+        ExchangeRate exchangeRate = exchangeRateDao.save(new ExchangeRate(baseCurrency.getId() , targetCurrency.getId(), rate));
+        return exchangeRateMapper.toDto(exchangeRate , baseCurrency , targetCurrency);
+
     }
 
 

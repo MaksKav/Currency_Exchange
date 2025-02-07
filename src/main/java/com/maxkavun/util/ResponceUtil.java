39c3ec7a88d4a6168b5251df4a12b1c95e7109dc
Max <kavun.onficial@gmail.com>
2025-02-07 14:22:25 +0100

    feat: introduce SendErrorResponse method to improve logic separation

--- src/main/java/com/maxkavun/util/ResponceUtil.java
+++ src/main/java/com/maxkavun/util/ResponceUtil.java
@@ -1,15 +1,25 @@
 package com.maxkavun.util;
 
+import com.google.gson.Gson;
+import com.maxkavun.dto.ExchangeErrorResponse;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
 public class ResponceUtil {
+    private static final Gson gson = new Gson();
+
     private ResponceUtil() {
     }
 
     public static  void sendResponse(HttpServletResponse response, int status, String message) throws IOException {
         response.setStatus(status);
         response.getWriter().write(message);
+    }
+
+    public static  void sendErrorResponse(HttpServletResponse response, int status, String errorMessage) throws IOException {
+        String errorJson = gson.toJson(new ExchangeErrorResponse(errorMessage));
+        response.setStatus(status);
+        response.getWriter().write(errorJson);
     }
 }

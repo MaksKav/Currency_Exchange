81a009c56080e53884d195ab39e37b987deec275
Max <kavun.onficial@gmail.com>
2025-02-07 14:19:17 +0100

    fix: update CurrencyValidator to better validation

--- src/main/java/com/maxkavun/validator/CurrencyValidator.java
+++ src/main/java/com/maxkavun/validator/CurrencyValidator.java
@@ -1,10 +1,17 @@
 package com.maxkavun.validator;
 
 public class CurrencyValidator {
-    public static boolean validateCurrencyData(String fullName , String code , String sign) {
+
+    public static boolean isValidCurrencieData(String fullName , String code , String sign) {
+        int MAX_NAME_LENGTH = 50;
+        int MAX_SIGN_LENGTH = 3;
+
         return fullName != null && !fullName.trim().isEmpty() &&
                code != null && !code.trim().isEmpty() &&
                sign != null && !sign.trim().isEmpty() &&
-               code.matches("^[A-Za-z]{3}$");
+               code.matches("^[A-Za-z]{3}$") &&
+               fullName.length() <= MAX_NAME_LENGTH &&
+               sign.length() <= MAX_SIGN_LENGTH;
+
     }
 }
